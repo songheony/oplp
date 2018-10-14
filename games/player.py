6f3ecbbb846933035a3f1ce59c6af1caaa838530
SongHeon <fefeff>
2018-10-14 14:01:26 +0900

    remove all contents in player.py

--- games/player.py
+++ games/player.py
@@ -1,27 +1,3 @@
-import numpy as np
-
-
 class Player:
-    def __init__(self, algorithm, experts, observer, shape):
-        self.algorithm = algorithm
-        self.experts = experts
-        self.observer = observer
-        self.shape = shape
-        self.observer.put('weights', self.algorithm.w)
-
-    def _advice(self, *args, **kwargs):
-        n = len(self.experts)
-        _return = np.zeros((n, self.shape))
-        for i in range(n):
-            _return[i, :] = np.array(self.experts[i].predict(*args, **kwargs))
-        self.observer.put('advice', _return)
-        return _return
-
     def predict(self, *args, **kwargs):
-        _return = np.dot(np.array(self.algorithm.w), self._advice(*args, **kwargs))
-        self.observer.put('prediction', _return)
-        return _return
-
-    def update(self, *args, **kwargs):
-        self.algorithm.update(*args, **kwargs)
-        self.observer.put('weights', self.algorithm.w)
+        raise NotImplementedError
