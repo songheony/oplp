5a488795e82c8d38206cd72d9830036d29d374cb
songheony <songheony@gmail.com>
2018-12-28 16:44:06 +0900

    add warning message and LSE for calculate

--- algorithms/waa_delayed.py
+++ algorithms/waa_delayed.py
@@ -1,6 +1,9 @@
 from __future__ import division
 from .algorithm import Algorithm
 import numpy as np
+import warnings
+import scipy.special as sc
+import sys
 
 
 class WAADelayed(Algorithm):
@@ -14,6 +17,8 @@ class WAADelayed(Algorithm):
         np_gradient_losses = np.array(gradient_losses)
         assert np_gradient_losses.shape[0] == self.w.shape[0]
         changes = lr * np_gradient_losses.sum(axis=1)
-        changes_max = np.max(changes)
-        wm = self.w * np.exp(-(changes - changes_max))
-        self.w = wm / np.sum(wm)               
+        # changes = changes - np.max(changes)
+        with warnings.catch_warnings():
+            warnings.filterwarnings('error')
+            temp = np.log(self.w + sys.float_info.min) - changes
+            self.w = np.exp(temp - sc.logsumexp(temp))      
